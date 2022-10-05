3a82d9e9d4a1ff4b2fa425b4bd07fa912534f0be
Tattisha <4artfloris@gmail.com>
2022-10-05 16:06:38 +0300

    Added loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,26 @@
 import React from "react";
 import axios from "axios";
+import { Oval } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
-    alert(`It is in Paris ${response.data.main.temp} C`);
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} C`
+    );
   }
   let apiKey = "e5329350d8d59ba6afe48bd7271155da";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello weather forecast</h2>;
+  return (
+    <Oval
+      ariaLabel="loading-indicator"
+      height={100}
+      width={100}
+      strokeWidth={5}
+      strokeWidthSecondary={1}
+      color="blue"
+      secondaryColor="white"
+    />
+  );
 }
