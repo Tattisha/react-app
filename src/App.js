076836b4ff42f29cd67c49451dcd331a5ab53b9e
Tattisha <4artfloris@gmail.com>
2022-10-11 13:45:39 +0300

    Added Weather.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-
-      <footer>
-        This project was coded by Tetiana Shapovalova and is{""}
-        <a href="https://github.com/Tattisha/react-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Tetiana Shapovalova and is {""}
+          <a href="https://github.com/Tattisha/react-app" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
