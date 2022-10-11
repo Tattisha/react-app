d3d85dc9590fcd2ce4bd87f40b44492b875278d8
Tattisha <4artfloris@gmail.com>
2022-10-11 12:50:53 +0300

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
 
-export default App;
+      <footer>
+        This project was coded by Tetiana Shapovalova and is{""}
+        <a href="https://github.com/Tattisha/react-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
