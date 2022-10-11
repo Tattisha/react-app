7af6e4ecf68f0bf868216011dd00201f1ee96cd5
Tattisha <4artfloris@gmail.com>
2022-10-11 12:19:09 +0300

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Weather city="Paris" />
-        <p>Hello from React.</p>
-      </header>
-    </div>
-  );
+  return <div className="App">Weather App</div>;
 }
 
 export default App;
