ffdd54620094cf493c08ac579819c1857cdcffb0
Tattisha <4artfloris@gmail.com>
2022-10-13 13:06:49 +0300

    Added conversion to fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,15 +14,17 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-2">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
-              <span className="temp">{Math.round(props.data.temperature)}</span>
-              <span className="unit">°C</span>
             </div>
           </div>
         </div>
+        <div className="col-4">
+          <WeatherTemperature celsius={props.data.temperature} />{" "}
+        </div>
+
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
